bbba4a641827a3ec29ba3a6b90ffb738c566bcab
Bilawal Ali <biluali84@gmail.com>
2023-10-06 00:35:19 +0500

    seventh commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://bilawal:C9rQjPUOoi5klXXA@cluster0.12dpeh0.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database')); 
